30dc3b14e78dd7a1d1f50bd2b852e8ff7cdefb8b
Joe Boud <Joe Boud>
2015-12-29 00:06:26 +0000

    first git commit from Derby

---
+++ ma furst projekt/src/HelloWorld.java
@@ -0,0 +1,12 @@
+import java.io.*;
+
+public class HelloWorld 
+{
+	public static void main(String[] args)
+	{
+		Console console = System.console();
+		Boolean sandwichManArrived = console.readLine();
+		if (sandwichManArrived);
+		System.out.println("Sandwich man's here");
+	}
+}
