f19b39c159af05ae741256fa94fb206453ccc50b
joeboud <joe.bouds@gmail.com>
2016-01-14 16:29:37 +0000

    testing docker connectivity

--- src/DBConnect.java
+++ src/DBConnect.java
@@ -11,10 +11,10 @@ public class DBConnect {
 	
 	String JDBC_DRIVER = "com.mysql.jdbc.Driver";
 	 
-	static String DB_URL = "jdbc:mysql://192.168.1.73:8888/ngbmaindb";
+	static String DB_URL = "jdbc:mysql://192.168.1.73:3306/ngbmaindb";
 
 	static String USER = "root";
-	static String PASS = "t3cni110n";
+	static String PASS = "netbuilder";
 
 		public void Connect(){
 			try {
